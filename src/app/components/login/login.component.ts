e8fe545ec2981d3db306f54b39d50286f020bce1
RaulMoya14 <Raul.Moya1@alu.uclm.es>
2024-03-18 19:30:54 +0100

    modified login and add home-component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { ButtonModule } from 'primeng/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,InputGroupAddonModule,InputGroupModule,ButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  username?: string;
+  password?: string;
+
+  constructor( private router:Router) { }
+
+  login() {
+    console.log('Username:', this.username);
+    console.log('Password:', this.password);
+    if (this.username == 'a' && this.password == 'a') {
+      console.log('Login successful');
+      this.router.navigate(['home']);
+    }
+
+  }
 }
