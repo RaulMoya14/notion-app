aacdd769568af50082444dbebceddb2ee1f600d6
RaulMoya14 <Raul.Moya1@alu.uclm.es>
2024-04-12 12:20:41 +0200

    pantallas para las colecciones

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { AddNoteComponent } from './components/add-note/add-note.component';
 import { ViewNoteComponent } from './components/view-note/view-note.component';
 import { RegisterComponent } from './components/register/register.component';
 import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import { CollectionNotesComponent } from './components/collection-notes/collection-notes.component';
 
 export const routes: Routes = [
 
@@ -15,6 +16,7 @@ export const routes: Routes = [
         { path: 'home', component: HomeComponent},
         { path: 'userNotes', component: ViewUserNotesComponent},
         { path: 'addNote', component: AddNoteComponent},
-        { path: 'viewNote/:id', component: ViewNoteComponent}
+        { path: 'viewNote/:id', component: ViewNoteComponent},
+        { path: 'collection', component: CollectionNotesComponent}
     ]}
 ];
--- src/app/components/collection-notes/collection-notes.component.ts
+++ src/app/components/collection-notes/collection-notes.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NotesService } from '../../services/notes.service';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-collection-notes',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule,ReactiveFormsModule],
   templateUrl: './collection-notes.component.html',
   styleUrl: './collection-notes.component.css'
 })
-export class CollectionNotesComponent {
+export class CollectionNotesComponent implements OnInit{
 
+  userNotes?:any[];
+  selectedNoteId: any = '';
+  collectionTitle: string = '';
+
+  constructor(private notesService: NotesService) {}
+
+  ngOnInit(): void {
+    let user = sessionStorage.getItem('userId') || '';
+    this.notesService.getNotes(user).subscribe((data) => {
+      console.log('User Notes:', data);
+      this.userNotes = data;
+    } );
+  }
+
+  selectNoteToAdd(noteId: any) {
+    this.selectedNoteId = noteId;
+  }
+
+  addNote() {
+    console.log(this.collectionTitle.valueOf())
+    if (!this.selectedNoteId || !this.collectionTitle) {
+      console.log('Please select a note and enter a collection title.');
+      return;
+    }
+    console.log('Selected Note ID:', this.selectedNoteId);
+    console.log('Collection Title:', this.collectionTitle);
+    this.selectedNoteId = '';
+    this.collectionTitle = '';
+  }
 }
