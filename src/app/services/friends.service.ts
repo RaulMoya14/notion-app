6439a5a693a49c7e4f350fd7eb65bf0cc4128835
RaulMoya14 <Raul.Moya1@alu.uclm.es>
2024-05-07 19:27:11 +0200

    screen friends

--- src/app/services/friends.service.ts
+++ src/app/services/friends.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpHeaders } from '@angular/common/http';
+import { ListPendingRequests } from '../models/list-pending-requests';
 
 @Injectable({
   providedIn: 'root'
@@ -13,28 +15,41 @@ export class FriendsService {
   }
 
   sendRequestFriend(user:string,requestedFriend:string){
+    let headers = new HttpHeaders();
+    headers.append('Content-Type','application/json');
     let sendRequestFriend = {
       friendsRequests: requestedFriend
     }
-    let body = JSON.stringify(sendRequestFriend);
-    return this.HttpClient.put(`http://localhost:3000/friends/sendFriendRequest/${user}`,body);
+    return this.HttpClient.put(`http://localhost:3000/friends/sendFriendRequest/${user}`,sendRequestFriend);
   }
 
   getFriends(user:string){
-    return this.HttpClient.get('http://localhost:3000/friends/admin');
+    return this.HttpClient.get(`http://localhost:3000/friends/${user}`);
   }
 
   removeFriend(user:string,friend:string){
     let removeFriend = {
-      user: user,
-      friend: friend
+      friends: friend
     }
-    let body = JSON.stringify(removeFriend);
-    return this.HttpClient.post('http://localhost:3000/deleteFriend',body);
+    return this.HttpClient.put(`http://localhost:3000/friends/deleteFriend/${user}`,removeFriend);
   }
 
   getPendingRequests(user:string){
-    return this.HttpClient.get(`http://localhost:3000/friends/requests/${user}`);
+    return this.HttpClient.get<ListPendingRequests>(`http://localhost:3000/friends/requests/${user}`);
+  }
+
+  acceptRequest(user:string,friend:string){
+    let acceptFriend = {
+      friendsRequests: friend
+    }
+    return this.HttpClient.put(`http://localhost:3000/friends/acceptFriend/${user}`,acceptFriend );
+  }
+
+  rejectRequest(user:string,friend:string){
+    let rejectRequest = {
+      friendsRequests: friend
+    }
+    return this.HttpClient.put(`http://localhost:3000/friends/declineFriend/${user}`,rejectRequest );
   }
 
 }
