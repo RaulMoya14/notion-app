3dc9e8f76c3a650d3f930c5ee5c2ecc59d5b6de5
RaulMoya14 <Raul.Moya1@alu.uclm.es>
2024-05-10 02:35:02 +0200

    sprint review 3

--- src/app/components/view-user-collection/view-user-collection.component.ts
+++ src/app/components/view-user-collection/view-user-collection.component.ts
@@ -2,21 +2,28 @@ import { CollectionService } from './../../services/collection.service';
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { NotesService } from '../../services/notes.service';
+import { MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-view-user-collection',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,ToastModule],
   templateUrl: './view-user-collection.component.html',
-  styleUrl: './view-user-collection.component.css'
+  styleUrl: './view-user-collection.component.css',
+  providers: [MessageService]
 })
 export class ViewUserCollectionComponent {
 
   idCollection: string = '';
   title: string = '';
   itemsList: any[] = [];
+  itemsListNotes: any[] = [];
 
-  constructor(private collectionService: CollectionService,private route: ActivatedRoute, private router:Router) { }
+  constructor(private collectionService: CollectionService,private route: ActivatedRoute, private router:Router,
+              private notesService:NotesService, private message:MessageService
+  ) { }
 
   ngOnInit(){
     this.getCollection();
@@ -40,6 +47,8 @@ export class ViewUserCollectionComponent {
   addInfoToListCollection(data:any){
     this.title = data[0].title;
     this.itemsList = data[0].notes;
+    this.itemsListNotes = this.getNamesNotes(this.itemsList);
+    console.log(this.itemsListNotes)
   }
 
   editNote(){
@@ -49,19 +58,40 @@ export class ViewUserCollectionComponent {
   deleteNote(note:any){
     this.collectionService.deleteNoteFromCollection(this.idCollection,note).subscribe({
       next: (data:any) => {
-        console.log(data);
+        this.message.add({severity:'success', summary:'Success', detail:'Note deleted in the shared collection'});
         this.getCollection();
       },
       error: (error:any) => {
+        this.message.add({severity:'error', summary:'Error', detail:'Error deleting note in the shared collection'});
         console.log(error);
       }
     });
   }
 
   seeNoteCollection(note:any){
+    console.log(note);
     this.router.navigate([`/viewNote/${note}`]);
   }
   goBack(){
     window.history.back();
+  }
+
+  getNamesNotes(notes:any){
+    let notesList:any[] = [];
+    notes.forEach((note:any) => {
+      this.notesService.getNote(note).subscribe({
+        next: (data:any) => {
+          notesList.push({
+            title:data[0].title,
+            id:data[0]._id
+          });
+        },
+        error: (error:any) => {
+          console.log(error);
+        }
+      });
+    });
+    return notesList;
+
   }
 }
